1dffb7b70a62b215964d8fbbe10f734e08ed9b0b
Cem <114831396+cemkusku@users.noreply.github.com>
2022-10-25 20:30:40 +0300

    Add files via upload

---
+++ KutuphaneProgrami/KutuphaneProgrami/EditBookForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KutuphaneProgrami
+{
+    public partial class EditBookForm : Form
+    {
+        MainForm _mainForm;
+        int currentIndex;
+        public EditBookForm(Kitap book, int index, MainForm mainForm)
+        {
+           
+            InitializeComponent();
+            _mainForm = mainForm;
+            currentIndex = index;
+            textBox1.Text = book.yazarAdı;
+            textBox2.Text = book.kitapAdı;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string updatedBookAuthor = textBox1.Text;
+            string updatedBookName = textBox2.Text;
+
+            Kitap updatedBook = new Kitap(updatedBookAuthor, updatedBookName);
+            _mainForm.updateListboxItem(updatedBook);
+            _mainForm.Show();
+            
+        }
+    }
+}
---
+++ KutuphaneProgrami/KutuphaneProgrami/KitapEkle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KutuphaneProgrami
+{
+    public partial class KitapEkle : Form
+    {
+        MainForm _mainForm;
+        public KitapEkle(MainForm mainForm)
+        {
+            InitializeComponent();
+            _mainForm = mainForm;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string yazarAdı = textBox1.Text;
+            string kitapAdı = textBox2.Text;
+
+            Kitap book = new Kitap(yazarAdı, kitapAdı);
+            _mainForm.addBook(book);
+            _mainForm.Show();
+            this.Hide();
+        }
+
+        private void AddBook_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
